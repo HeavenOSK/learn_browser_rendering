1dab9d25086c678c8eeffd32edce1d1bc53a3f82
HeavenOSK <heavenosk@gmail.com>
2022-01-15 12:47:35 +0900

    add: sample html css files

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
-const exec = async () => {
+import {promises as fs} from 'fs';
 
+const htmlPath = './src/milestones/0000/sample.html';
+const cssPath = './src/milestones/0000/sample.css';
+
+const exec = async () => {
+  const html = await fs.readFile(htmlPath, 'utf-8');
+  const css = await fs.readFile(cssPath, 'utf-8');
+  console.log(html)
+  console.log(css)
 }
 exec()
   .then(message => console.log('success', message))
